5c7e9a0364056af3c2945909cf4624011ce7ba8e
mickey0142 <59070142@it.kmitl.ac.th>
2019-04-18 23:18:44 +0700

    init repo

---
+++ request/index.js
@@ -0,0 +1,20 @@
+
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const winston = require("winston");
+
+app.use(urlencodeParser = bodyParser.urlencoded ({extended: false}));
+app.use(bodyParser.json());
+
+require("./startup/logging.js")();
+require("./startup/db.js")();
+require("./startup/routes.js")(app);
+require("./startup/eureka.js")();
+
+const port = process.env.PORT || 3000;
+const server = app.listen(port, ()=>{
+    console.log("connected to port " + port + " :3");
+});
+
+module.exports = server;
